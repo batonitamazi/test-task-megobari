0aad6043f4bcf7201c33645b63831c8273e4724f
batonitamazi <tamazi.mirianashvili.1@btu.edu.ge>
2022-09-13 17:56:55 +0400

    design fixes, added slider

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 
-
+//navbar
 const nav = document.querySelector('.nav');
-
-
-
-
 const header = document.querySelector('.header');
 const navHeight = nav.getBoundingClientRect().height;
 
 const stickyNav = function (entries) {
   const [entry] = entries;
-  // console.log(entry);
 
   if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
@@ -24,24 +19,44 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 
 headerObserver.observe(header);
 
-// const arrowButton = document.querySelector(".dropdown__btn");
-// arrowButton.addEventListener('click', () => {
-//   document.querySelector(".hidden__content").classList.toggle("show");
+//dropdown menu
 
-// })
-// dropdown menu
 const content = document.querySelectorAll(".hidden__content");
 const arrow = document.querySelectorAll(".dropdown__btn");
 
 
-for(let i=0; i<arrow.length; i++) {
-  arrow[i].addEventListener('click', function() {
+for (let i = 0; i < arrow.length; i++) {
+  arrow[i].addEventListener('click', function () {
     content[i].classList.toggle("show");
     this.classList.toggle("rotate");
   })
-  
+
+}
+
+//slider
+let arr = [0, 1, 3, 4]
+let slideIndex = 1;
+if (window.innerWidth < 500) {
+  showSlides(slideIndex);
 }
 
 
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-
+function showSlides(n) {
+  let i;
+  let slides = document.querySelectorAll(".user__card");
+  let dots = document.querySelectorAll(".dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
